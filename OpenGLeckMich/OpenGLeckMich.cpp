c81338e59d87a34db840b1a3417c275d814cb0ad
foxeltm <fuchs.raffael@hotmail.com>
2025-12-18 19:32:00 +0100

    Vertex Attributes and Layouts

--- OpenGLeckMich/OpenGLeckMich.cpp
+++ OpenGLeckMich/OpenGLeckMich.cpp
@@ -1,4 +1,6 @@
-﻿#include <GLFW/glfw3.h>
+﻿#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
 
 int main(void)
 {
@@ -7,9 +9,10 @@ int main(void)
     /* Initialize the library */
     if (!glfwInit())
         return -1;
-
+    
+    
     /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    window = glfwCreateWindow(640, 480, "Triangle", NULL, NULL);
     if (!window)
     {
         glfwTerminate();
@@ -19,11 +22,32 @@ int main(void)
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
 
+    if (glewInit() != GLEW_OK)
+        std::cout << "ERROR! " << std::endl;
+
+    float positions[6] = {
+        -0.5f, -0.5f,
+         0.0f,  0.5f,
+         0.5f, -0.5f
+    };
+
+    unsigned int buffer;
+    glGenBuffers(1, &buffer);
+    glBindBuffer(GL_ARRAY_BUFFER, buffer);
+    glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STATIC_DRAW);
+
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0);
+
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
+
+        glDrawArrays(GL_TRIANGLES, 0, 3);
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
