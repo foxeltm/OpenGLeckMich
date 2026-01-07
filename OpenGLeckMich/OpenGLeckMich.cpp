4f827a6cc177f57153a8fe5cea937e91dbc40d90
foxeltm <fuchs.raffael@hotmail.com>
2026-01-07 21:13:34 +0100

    mvp

--- OpenGLeckMich/OpenGLeckMich.cpp
+++ OpenGLeckMich/OpenGLeckMich.cpp
@@ -86,14 +86,15 @@ int main(void)
         IndexBuffer ib(indicies, 6);
 
 		glm::mat4 proj = glm::ortho(0.0f, 960.0f, 0.0f, 540.0f , -1.0f, 1.0f);
-		glm::vec4 vp(100.0f, 100.0f, 0.0f, 1.0f);
+		glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(-100, 0, 0));
+		glm::mat4 model = glm::translate(glm::mat4(1.0f), glm::vec3(200, 200, 0));
 
-		glm::vec4 result = proj * vp;
+		glm::mat4 mvp = proj * view * model;
 
         Shader shader("../OpenGLeckMich/res/shaders/Basic.Shader");
 		shader.Bind();
 		shader.SetUniform4f("u_Color", 0.616f, 0.0f, 1.0f, 1.0f);
-		shader.SetUniformMat4f("u_MVP", proj);
+		shader.SetUniformMat4f("u_MVP", mvp);
 
 		Texture texture("res/textures/terraria.png");
 		texture.Bind();
