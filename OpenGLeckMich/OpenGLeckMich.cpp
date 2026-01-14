03cc0ca499a1b988e1db9bfff15a4ff92da07657
foxel_tm <fuchs.raffael@hotmail.com>
2026-01-14 21:09:37 +0100

    made a second texture

--- OpenGLeckMich/OpenGLeckMich.cpp
+++ OpenGLeckMich/OpenGLeckMich.cpp
@@ -64,10 +64,10 @@ int main(void)
         std::cout << "ERROR! " << std::endl;
     {
         float positions[16] = {
-            100.0f, 100.0f, 0.0f, 0.0f,//0
-            200.0f, 100.0f, 1.0f, 0.0f,//1
-            200.0f, 200.0f, 1.0f, 1.0f,//2
-            100.0f, 200.0f, 0.0f, 1.0f//3
+            -50.0f, -50.0f, 0.0f, 0.0f,//0
+             50.0f, -50.0f, 1.0f, 0.0f,//1
+             50.0f,  50.0f, 1.0f, 1.0f,//2
+            -50.0f,  50.0f, 0.0f, 1.0f//3
         };
 
         u32 indicies[] =
@@ -93,7 +93,7 @@ int main(void)
         IndexBuffer ib(indicies, 6);
 
 		glm::mat4 proj = glm::ortho(0.0f, 960.0f, 0.0f, 540.0f , -1.0f, 1.0f);
-		glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(-100, 0, 0));
+		glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(0, 0, 0));
 
         Shader shader("../OpenGLeckMich/res/shaders/Basic.Shader");
 		shader.Bind();
@@ -117,7 +117,8 @@ int main(void)
 		ImGui::StyleColorsDark();
 		//ImGui::StyleColorsClassic();
 
-		glm::vec3 translation(200, 200, 0);
+		glm::vec3 translationA(200, 200, 0);
+        glm::vec3 translationB(400, 200, 0);
 
         //red
         float r = 0.0f;
@@ -132,14 +133,27 @@ int main(void)
 			ImGui_ImplGlfw_NewFrame();
             ImGui::NewFrame();
 
-            glm::mat4 model = glm::translate(glm::mat4(1.0f), translation);
-            glm::mat4 mvp = proj * view * model;
+            {
+                glm::mat4 model = glm::translate(glm::mat4(1.0f), translationA);
+                glm::mat4 mvp = proj * view * model;
+                shader.Bind();
+                shader.SetUniform4f("u_Color", r, 0.0f, 1.0f, 1.0f);
+                shader.SetUniformMat4f("u_MVP", mvp);
 
-            shader.Bind();
-            shader.SetUniform4f("u_Color", r, 0.0f, 1.0f, 1.0f);
-			shader.SetUniformMat4f("u_MVP", mvp);
+                renderer.Draw(va, ib, shader);
+            }
 
-			renderer.Draw(va, ib, shader);
+
+            {
+                glm::mat4 model = glm::translate(glm::mat4(1.0f), translationB);
+                glm::mat4 mvp = proj * view * model;
+                shader.Bind();
+                shader.SetUniform4f("u_Color", r, 0.0f, 1.0f, 1.0f);
+                shader.SetUniformMat4f("u_MVP", mvp);
+
+                renderer.Draw(va, ib, shader);
+            }
+
             //red
             if (r > 1.0f)
                 incrementR = -0.05f;
@@ -150,7 +164,9 @@ int main(void)
 
             
             {
-                ImGui::SliderFloat3("Translation", &translation.x, 0.0f, 960.0f);
+                ImGui::SliderFloat3("Translation A", &translationA.x, 0.0f, 960.0f);
+                ImGui::SliderFloat3("Translation B", &translationB.x, 0.0f, 960.0f);
+
 				//ImGui::Text("counter = %d", counter); lol 
 
             }
