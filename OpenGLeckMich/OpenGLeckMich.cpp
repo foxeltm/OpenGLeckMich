a14e1e3dda680572ef4a85993397cd13c33908f6
foxel_tm <fuchs.raffael@hotmail.com>
2026-01-26 20:58:53 +0100

    made a deacent framework but stil LINK error

--- OpenGLeckMich/OpenGLeckMich.cpp
+++ OpenGLeckMich/OpenGLeckMich.cpp
@@ -25,6 +25,8 @@
 #include "backends/imgui_impl_glfw.h"
 #include "backends/imgui_impl_opengl3.h"
 
+#include "src/Test/TestClearColor.h"
+
 typedef uint8_t u8;
 typedef uint16_t u16;
 typedef uint32_t u32;
@@ -66,50 +68,9 @@ int main(void)
 	//glewExperimental = GL_TRUE; cooler
 
     {
-        float positions[16] = {
-            -50.0f, -50.0f, 0.0f, 0.0f,//0
-             50.0f, -50.0f, 1.0f, 0.0f,//1
-             50.0f,  50.0f, 1.0f, 1.0f,//2
-            -50.0f,  50.0f, 0.0f, 1.0f//3
-        };
-
-        u32 indicies[] =
-        {
-            0, 1, 2,
-            2, 3, 0
-        };
 
         GLCall(glEnable(GL_BLEND));
 		GLCall(glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA));
-		
-
-		VertexArray va;
-        VertexBuffer vb(positions, sizeof(positions));
-
-        VertexBufferLayout layout;
-		layout.Push<float>(2);
-		layout.Push<float>(2);
-		va.AddBuffer(vb, layout);
-        
-
-        IndexBuffer ib(indicies, 6);
-
-		glm::mat4 proj = glm::ortho(0.0f, 960.0f, 0.0f, 540.0f , -1.0f, 1.0f);
-		glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(0, 0, 0));
-
-        Shader shader("../OpenGLeckMich/res/shaders/Basic.Shader");
-		shader.Bind();
-		shader.SetUniform4f("u_Color", 0.616f, 0.0f, 1.0f, 1.0f);
-
-		Texture texture("res/textures/terraria.png");
-		texture.Bind();
-        shader.SetUniform1i("u_Texture", 0);
-		//papa ist hier
-
-		va.Unbind();
-		vb.Unbind();
-		ib.Unbind();
-        shader.Unbind();
 
 		Renderer renderer;
 
@@ -117,65 +78,20 @@ int main(void)
 		ImGui_ImplGlfw_InitForOpenGL(window, true);
         ImGui_ImplOpenGL3_Init("#version 330");
 		ImGui::StyleColorsDark();
-		//ImGui::StyleColorsClassic();
 
-		glm::vec3 translationA(200, 200, 0);
-        glm::vec3 translationB(400, 200, 0);
+        test::TestClearColor test;
 
-        //red
-        float r = 0.0f;
-        float incrementR = 0.05f;
-        /* Loop until the user closes the window */
         while (!glfwWindowShouldClose(window))
         {
             /* Render here */
 			renderer.Clear();
 
-            ImGui_ImplOpenGL3_NewFrame();
-			ImGui_ImplGlfw_NewFrame();
-            ImGui::NewFrame();
-
-            {
-                glm::mat4 model = glm::translate(glm::mat4(1.0f), translationA);
-                glm::mat4 mvp = proj * view * model;
-                shader.Bind();
-                shader.SetUniform4f("u_Color", r, 0.0f, 1.0f, 1.0f);
-                shader.SetUniformMat4f("u_MVP", mvp);
-
-                renderer.Draw(va, ib, shader);
-            }
-
-
-            {
-                glm::mat4 model = glm::translate(glm::mat4(1.0f), translationB);
-                glm::mat4 mvp = proj * view * model;
-                shader.Bind();
-                shader.SetUniform4f("u_Color", r, 0.0f, 1.0f, 1.0f);
-                shader.SetUniformMat4f("u_MVP", mvp);
-
-                renderer.Draw(va, ib, shader);
-            }
-
-            //red
-            if (r > 1.0f)
-                incrementR = -0.05f;
-            else if (r < 0.0f)
-                incrementR = 0.06f;
-
-            r += incrementR;
-
-            
-            {
-                ImGui::SliderFloat3("Translation A", &translationA.x, 0.0f, 960.0f);
-                ImGui::SliderFloat3("Translation B", &translationB.x, 0.0f, 960.0f);
-
-				//ImGui::Text("counter = %d", counter); lol 
-            }
+            test.OnUpdate(0.0f);
+			test.OnRender();
            
-            //ImGui cool
 			ImGui::Render();
 			ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
-
+			test.OnImGuiRender();
             /* Swap front and back buffers */
             glfwSwapBuffers(window);
 
